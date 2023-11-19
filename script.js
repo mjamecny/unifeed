c464e2f9f44d327467ca85c46639e7e5b44be8f5
mjamecny <jamecny.m@tutanota.com>
2023-11-19 11:49:29 +0100

    add type for script

--- script.js
+++ script.js
@@ -1,8 +1,6 @@
 const hamburgerElement = document.querySelector(".hamburger")
 const navElement = document.querySelector(".nav")
 
-console.log(hamburgerElement)
-
 hamburgerElement.addEventListener("click", () => {
   navElement.classList.toggle("hidden")
 })
